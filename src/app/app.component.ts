3408851cd6314a64a27a5a210d12f69646578eb1
Abhinav0930 <murthy0930@gmail.com>
2019-05-29 23:33:59 +0200

    adding ne properties to columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   title = 'app';
 
     columnDefs = [
-        {headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true },
-        {headerName: 'Model', field: 'model' },
-        {headerName: 'Price', field: 'price'}
+        {headerName: 'Make', field: 'make', sortable: true, filter: true },
+        {headerName: 'Model', field: 'model', sortable: true, filter: true },
+        {headerName: 'Price', field: 'price', sortable: true, filter: true}
     ];
 
     rowData = [
